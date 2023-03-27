b260239534e629c342b0ce216b9ae513f934463c
MakLien <112383602+MakLien@users.noreply.github.com>
2023-03-27 10:53:50 +0200

    PictureBox Övning kod

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Image.FromFile(@"C:\Users\Maksym.Bodnar\Pictures\profile.png");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filepath = openFileDialog1.FileName;
+            pictureBox3.Image = Image.FromFile(filepath);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox6.Image = Image.FromFile(@"C:\Users\Maksym.Bodnar\Downloads\sleep.jpg");
+        }
+    }
+}
